e9feccbd04aebe0e3d7c75800b56604b82a33827
edwinnlunaa <165944885+edwinnlunaa@users.noreply.github.com>
2024-07-09 01:39:38 -0400

    Finished Very Easy Part

--- gi_15/src/App.js
+++ gi_15/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Name: Edwin Luna</h1>
+        <p>Phone : 704-999-2330</p>
+        <p>Email: edwinlunadlg@gmail.com</p>
+        <p>Date of Birth: 09-30-1999</p>
+        <p>Occupation: Coding Apprentice</p>
+      </div>
+    );
+  }
 }
 
 export default App;
