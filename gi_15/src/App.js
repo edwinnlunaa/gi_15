7de5ba10a91a628e569bb742b43230decbae54eb
edwinnlunaa <165944885+edwinnlunaa@users.noreply.github.com>
2024-07-09 01:42:08 -0400

    Finished Easy Part

--- gi_15/src/App.js
+++ gi_15/src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {}
+    };
+  }
+  
   render() {
     return (
       <div>
